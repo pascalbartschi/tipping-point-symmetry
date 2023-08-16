364f167b247fcb07ec5578024dd0124cba7d2667
pascalbartschi <paesce.26@gmail.com>
2023-08-16 11:06:04 +0200

    estimated algo complexity

---
+++ experiments/estimate_algo_complexity.R
@@ -0,0 +1,28 @@
+# estimate algorithmic complexity
+
+source("analysis/read_microxanox.R")
+
+wts <- c(1e2, 1e3, 1e4)
+steps <- 300
+times <- rep(NA, times = 3)
+c = 0
+
+
+for (wt in wts){
+  c <- c + 1
+  wait_time <- wt
+  source("experiments/setup_sym_experiment_temporal.R")
+  a <- Sys.time()
+  run_temporal_ssfind_symmetric(parameter)
+  b <- Sys.time()
+  times[c] <- b - a
+  
+}
+
+df <- data.frame(duration = wts * steps, 
+                 times = times)
+
+ggplot(data = df, mapping = aes(x = log10(duration), y = times)) + 
+  geom_point() + 
+  labs(x = "log10(duration)", y = "time[s]") +
+  theme_bw()
